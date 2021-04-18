a24c97341fabf815fc616b534f8535d0ef95bb3d
LaseronixFD <82584049+LaseronixFD@users.noreply.github.com>
2021-04-18 22:17:47 +0300

    Add files via upload

--- ArabBot.py
+++ ArabBot.py
@@ -8,21 +8,22 @@ from translate import Translator
 class ArabBot(dc.Client):
     async def on_message(self, message):
         m = '{0.content}'.format(message)
-        if m[0] == '&':
-            b_m = ''
-            for i in range(len(m)-1):
-                b_m += m[i+1]
-            tr = Translator(from_lang='Russian', to_lang='Arabic')
-            b_answer = tr.translate(b_m)
-            print(b_answer)
-            await message.channel.send(b_answer)
-        if m[0] == '%':
-            b_m = ''
-            for i in range(len(m)-1):
-                b_m += m[i+1]
-            tr = Translator(from_lang='Arabic', to_lang='Russian')
-            b_answer = tr.translate(b_m)
-            print(b_answer)
-            await message.channel.send(b_answer)
+        if len(m) > 0:
+            if m[0] == '&':
+                b_m = ''
+                for i in range(len(m)-1):
+                    b_m += m[i+1]
+                tr = Translator(from_lang='Russian', to_lang='Arabic')
+                b_answer = tr.translate(b_m)
+                print(b_answer)
+                await message.channel.send(b_answer)
+            if m[0] == '%':
+                b_m = ''
+                for i in range(len(m)-1):
+                    b_m += m[i+1]
+                tr = Translator(from_lang='Arabic', to_lang='Russian')
+                b_answer = tr.translate(b_m)
+                print(b_answer)
+                await message.channel.send(b_answer)
 
-ArabBot().run('ODMxMTQxNTU5OTczMDUyNDM2.YHQ65Q.P3UDdRJCMDrlC2JX1A7C3Z5SRDo')
+ArabBot().run('ODMxMTQxNTU5OTczMDUyNDM2.YHQ65Q.XXlpTNQTm7xwCFrwqn3Qa4mNTVY')
